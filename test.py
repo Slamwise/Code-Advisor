9562a32fe4ad1a5b11a5c9e4cee10d3a7f72f698
Slamwise <smsa2222@yahoo.com>
2024-02-06 22:13:50 -0500

    0.1.5

--- test.py
+++ test.py
@@ -1,9 +1,24 @@
 # your_script.py
+import random
 @profile
-def main():
+def main(n):
     # Generate a list of random numbers
-    data = [x**(x+2) for x in range(0,100)]
-    print(data)
+    x_list = []
+    y_list = []
+    z_list = []
+    xyz_list = []
+    for x in range(1, n):
 
+        y = x^2
+        z = y/random.randint(1, x)
+
+        x_list.append(x)
+        y_list.append(y)
+        z_list.append(z)
+
+
+        xyz_list.append((x, y, z))
+
+    print(xyz_list)
 if __name__ == "__main__":
-    main()
+    main(10000)
